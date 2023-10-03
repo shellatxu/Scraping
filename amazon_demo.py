fef1f4f0ba6c12a96c998e6ea425cb33fa899007
Shell XU <31234031+shellatxu@users.noreply.github.com>
2023-10-03 19:57:39 +0800

    Update amazon_demo.py
    request ssl

--- amazon_demo.py
+++ amazon_demo.py
@@ -1,54 +1,58 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from seleniumwire import webdriver 
-from seleniumwire.utils import decode
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
-from selenium.webdriver.common.keys import Keys
+import time,re
+import requests
+import ssl
+import requests
 
-def add_product_to_cart(url, keyword):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    #options.add_argument("user-data-dir=/home/shell/.config/google-chrome/")
-    options.add_argument("profile-directory=Default")
-    options.add_argument("--disable-blink-features=AutomationControlled") 
-    options.add_experimental_option("excludeSwitches", ["enable-automation","enable-logging"]) 
-    options.add_experimental_option("useAutomationExtension", False)
-    options.add_argument("disable-extensions")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    options.add_argument('–log-level=3')
-    #options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
+from requests.adapters import HTTPAdapter
+from urllib3.poolmanager import PoolManager
+from urllib3.util.ssl_ import create_urllib3_context
 
-    driver.get(url)
-    time.sleep(5)
+# see "openssl ciphers" command for cipher names
+CIPHERS = "ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384"
 
-    try:
-        driver.find_elements(By.XPATH, '//*[@id="sp-cc-accept"]')[0].click()
-    except:
-        print('not found accept cookie button')        
-        
-    try:
-        driver.find_elements(By.XPATH, '//*[@id="twotabsearchtextbox"]')[0].send_keys(keyword)
-        driver.find_elements(By.XPATH, '//*[@id="twotabsearchtextbox"]')[0].send_keys(Keys.ENTER)
-    except:
-        print('not found search input')
-    try:
-        driver.find_elements(By.XPATH, '//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a')[0].click()
-    except:
-        print('not found search result')    
-    
-    try:
-        driver.find_elements(By.XPATH, '//*[@id="add-to-cart-button-ubb"]')[0].click()
-    except:
-        print('not found add to cart button')      
+
+class TlsAdapter(HTTPAdapter):
+    def __init__(self, ssl_options=0, **kwargs):
+        self.ssl_options = ssl_options
+        super(TlsAdapter, self).__init__(**kwargs)
+
+    def init_poolmanager(self, *pool_args, **pool_kwargs):
+        ctx = create_urllib3_context(ciphers=CIPHERS, cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
+        self.poolmanager = PoolManager(*pool_args, ssl_context=ctx, **pool_kwargs)
+
 
 
 if __name__ == "__main__":
-    start_url = 'https://www.amazon.co.uk/'
+    
     keyword = 'iphone'
-    add_product_to_cart(start_url, keyword)
+    site_home_url = 'https://www.amazon.co.uk'
+    start_url = 'https://www.amazon.co.uk/s?k='+keyword
+    
+    adapter = TlsAdapter(ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1)
+    session = requests.session()
+    session.mount("https://", adapter)
+    
+    headers =  {
+        "authority" : "www.amazon.co.uk",
+        "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language" : "zh-CN,zh;q=0.9",
+        "sec-ch-ua" : '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+        "sec-ch-ua-mobile" : "?0",
+        "sec-ch-ua-platform" : '"Windows"',
+        "sec-fetch-dest" : "document",
+        "sec-fetch-mode" : "navigate",
+        "sec-fetch-site" : "none",
+        "sec-fetch-user" : "?1",
+        "upgrade-insecure-requests" : "1",
+        "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    }  
+    res = session.get(start_url, headers=headers)
+    html = res.text.encode('utf-8')
+    
+    search_items = re.findall('<a class="a-link-normal s-no-outline" href="([^\"]*)">', html.decode('utf-8'))
+    search_items_array = []
+    if search_items:
+        target_item_url = site_home_url+search_items[0]
+        res =  session.get(target_item_url, headers=headers)
+        html = res.text.encode('utf-8')
+        print(html)
