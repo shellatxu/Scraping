cc71f5fb25c6e0cfa3defc3279b5bb7bffdc6328
Shell XU <31234031+shellatxu@users.noreply.github.com>
2023-10-03 20:45:06 +0800

    Update amazon_demo.py
    get first product from search list and add to cart
    print cart item count number at last.

--- amazon_demo.py
+++ amazon_demo.py
@@ -1,8 +1,7 @@
-import time,re
+import re
 import requests
 import ssl
-import requests
-
+from bs4 import BeautifulSoup
 from requests.adapters import HTTPAdapter
 from urllib3.poolmanager import PoolManager
 from urllib3.util.ssl_ import create_urllib3_context
@@ -20,7 +19,57 @@ class TlsAdapter(HTTPAdapter):
         ctx = create_urllib3_context(ciphers=CIPHERS, cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
         self.poolmanager = PoolManager(*pool_args, ssl_context=ctx, **pool_kwargs)
 
-
+def get_form_details(form):
+    details = {}
+    action = form.attrs.get("action").lower()
+    method = form.attrs.get("method", "get").lower()
+    inputs = []
+    for input_tag in form.find_all("input"):
+        # get type of input form control
+        input_type = input_tag.attrs.get("type", "text")
+        # get name attribute
+        input_name = input_tag.attrs.get("name")
+        # get the default value of that input tag
+        input_value =input_tag.attrs.get("value", "")
+        # add everything to that list
+        inputs.append({"type": input_type, "name": input_name, "value": input_value})
+        
+    for select in form.find_all("select"):
+        # get the name attribute
+        select_name = select.attrs.get("name")
+        # set the type as select
+        select_type = "select"
+        select_options = []
+        # the default select value
+        select_default_value = ""
+        # iterate over options and get the value of each
+        for select_option in select.find_all("option"):
+            # get the option value used to submit the form
+            option_value = select_option.attrs.get("value")
+            if option_value:
+                select_options.append(option_value)
+                if select_option.attrs.get("selected"):
+                    # if 'selected' attribute is set, set this option as default    
+                    select_default_value = option_value
+        if not select_default_value and select_options:
+            # if the default is not set, and there are options, take the first option as default
+            select_default_value = select_options[0]
+        # add the select to the inputs list
+        inputs.append({"type": select_type, "name": select_name, "values": select_options, "value": select_default_value})
+    for textarea in form.find_all("textarea"):
+        # get the name attribute
+        textarea_name = textarea.attrs.get("name")
+        # set the type as textarea
+        textarea_type = "textarea"
+        # get the textarea value
+        textarea_value = textarea.attrs.get("value", "")
+        # add the textarea to the inputs list
+        inputs.append({"type": textarea_type, "name": textarea_name, "value": textarea_value})
+    # put everything to the resulting dictionary
+    details["action"] = action
+    details["method"] = method
+    details["inputs"] = inputs
+    return details
 
 if __name__ == "__main__":
     
@@ -49,10 +98,52 @@ if __name__ == "__main__":
     res = session.get(start_url, headers=headers)
     html = res.text.encode('utf-8')
     
+    # find product list items
     search_items = re.findall('<a class="a-link-normal s-no-outline" href="([^\"]*)">', html.decode('utf-8'))
     search_items_array = []
     if search_items:
+        # just get the first product
         target_item_url = site_home_url+search_items[0]
         res =  session.get(target_item_url, headers=headers)
         html = res.text.encode('utf-8')
-        print(html)
+        soup = BeautifulSoup(html, "html.parser")
+        
+        # get the add to cart form
+        all_forms = soup.find_all("form")
+
+        fd = get_form_details(all_forms[2])
+
+        add_cart_url = site_home_url+fd['action']
+        
+        data = {}
+        
+        for input_tag in fd["inputs"]:
+            if input_tag["type"] == "hidden":
+                data[input_tag["name"]] = input_tag["value"]
+            elif input_tag["type"] == "select":
+                for i, option in enumerate(input_tag["values"], start=1):
+                    if option == input_tag["value"]:
+                        print(f"{i} # {option} (default)")
+                    else:
+                        print(f"{i} # {option}")
+                choice = input(f"Enter the option for the select field '{input_tag['name']}' (1-{i}): ")
+                try:
+                    choice = int(choice)
+                except:
+                    value = input_tag["value"]
+                else:
+                    value = input_tag["values"][choice-1]
+                data[input_tag["name"]] = value
+            elif input_tag["type"] != "submit":
+                value = input(f"Enter the value of the field '{input_tag['name']}' (type: {input_tag['type']}): ")
+                data[input_tag["name"]] = value        
+
+        # submit add cart form 
+        res = session.post(add_cart_url, data=data, headers=headers)
+        html = res.text.encode('utf-8')
+
+        # get the cart items number
+        cart_item_count = re.findall('<span id="nav-cart-count" aria-hidden="true" class="nav-cart-count nav-cart-1 nav-progressive-attribute nav-progressive-content">([\d]*)</span>', html.decode('utf-8'))
+        print(cart_item_count)
+        
+        
